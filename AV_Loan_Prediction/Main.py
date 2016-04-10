729d7978eb1e1186bf800c71ca24a591f68778ed
arnabdutta <arnab.dutta@gmail.com>
2016-04-11 00:55:28 +0200

    restructured

--- AV_Loan_Prediction/Main.py
+++ AV_Loan_Prediction/Main.py
@@ -1,34 +1,46 @@
 import pandas as pd
-from sklearn import preprocessing
-from sklearn.cross_validation import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn import cross_validation
 from scipy.stats import mode
-import xgboost as xgb
+from sklearn import cross_validation
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier
 
-def main():
+
+# fit an algorithm and cross validate
+def algo_fit_cross_validated(training_matrix, target):
+    # Random Forest
+    rf = RandomForestClassifier(n_estimators=200, max_depth=6)
+
+    scores = cross_validation.cross_val_score(rf, training_matrix, target, cv=5)
+    print("Accuracy: %0.4f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    rf.fit(training_matrix, target)
+
+    return rf
+
+
+# encode labels to numeric values
+def encode(df_train, df_test):
     le_sex = preprocessing.LabelEncoder()
+    feature_matrix = df_train.iloc[:, 0:12]
+    target_matrix = df_train.iloc[:, 12]
+    encoded_train_feature_matrix = feature_matrix.apply(le_sex.fit_transform)
+    encoded_test_feature_matrix = df_test.apply(le_sex.fit_transform)
+    return encoded_train_feature_matrix, target_matrix, encoded_test_feature_matrix
 
-    df_train = pd.read_csv('train.csv')
-    # print(df_train.shape)
+
+# read the training file and test files to create preprocessed dataframes
+def preprocess(train_file, test_file):
+    df_train = pd.read_csv(train_file)
 
     # missing values imputing
     df_train["LoanAmount"].fillna(df_train["LoanAmount"].mean(), inplace=True)
     df_train["Loan_Amount_Term"].fillna(df_train["Loan_Amount_Term"].mean(), inplace=True)
     df_train["Credit_History"].fillna(df_train["Credit_History"].mean(), inplace=True)
-    zz = mode(df_train['Gender'])
-    # print(mode(df_train['Gender'])[0][0])
 
     df_train['Gender'].fillna(mode(df_train['Gender'])[0][0], inplace=True)
     df_train['Married'].fillna(mode(df_train['Married'])[0][0], inplace=True)
     df_train['Self_Employed'].fillna(mode(df_train['Self_Employed'])[0][0], inplace=True)
 
-    # df_train = df_train.fillna('-')
-    # print(df_train.shape)
-
-    df_test = pd.read_csv('test.csv')
-    # print(len(df_test))
+    df_test = pd.read_csv(test_file)
     df_test["LoanAmount"].fillna(df_test["LoanAmount"].mean(), inplace=True)
     df_test["Loan_Amount_Term"].fillna(df_test["Loan_Amount_Term"].mean(), inplace=True)
     df_test["Credit_History"].fillna(df_test["Credit_History"].mean(), inplace=True)
@@ -37,46 +49,30 @@ def main():
     df_test['Married'].fillna(mode(df_test['Married'])[0][0], inplace=True)
     df_test['Self_Employed'].fillna(mode(df_test['Self_Employed'])[0][0], inplace=True)
 
-    # df_test = df_test.fillna('-')
+    return df_train, df_test
 
-    original_id = df_test.iloc[:, 0]
-    test_encoded = df_test.apply(le_sex.fit_transform)
-    # print(len(df_test))
 
-    X = df_train.iloc[:, 0:12]
-    z = df_train.iloc[:, 12]
-    # y = df_train.iloc[:, 12].reshape(len(df_train), 1)
-    # y = y.apply(lambda x: x[0])
-    # print(X.shape)
-    X_encoded = X.apply(le_sex.fit_transform)
-    #
-    # for est in range(100, 2000, 100):
-    #     for dep in range(3, 10):
-    rf = RandomForestClassifier(n_estimators=200, max_depth=6)
-    # gbm = xgb.XGBClassifier(max_depth=7, n_estimators=100, learning_rate=0.05).fit(X_encoded, z)
-
-    # rf.fit(X_train_encoded, y_train)
-
-    scores = cross_validation.cross_val_score(rf, X_encoded, z, cv = 5)
-    # print("ESt = {}, Dep = {}, Score = {}".format(est, dep, scores.mean()))
-    print("Accuracy: %0.4f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
-    # y_pred = rf.predict(X_test_encoded)
-    # score = accuracy_score(y_test, y_pred)
-
-    # print(score)
-    rf.fit(X_encoded, z)
-    Loan_ID = test_encoded.iloc[:, 0]
-    Loan_Status = rf.predict(test_encoded)
-
-    # print(len(Loan_ID))
-    # print(len(Loan_Status))
-    # print(df_test.shape)
-
-    final_preds = pd.DataFrame({'Loan_ID': original_id,
-                                    'Loan_Status': pd.Series(Loan_Status)}, columns=['Loan_ID', 'Loan_Status'])
+# run the fitted algo on the test
+def predict_save_file(df_test, test_encoded, algo):
+    final_preds = pd.DataFrame({'Loan_ID': df_test.iloc[:, 0],
+                                'Loan_Status': pd.Series(algo.predict(test_encoded))},
+                               columns=['Loan_ID', 'Loan_Status'])
 
     final_preds.to_csv('data.csv', header=True, index=False)
+
+
+def main():
+    # clean up the inputs
+    df_train, df_test = preprocess('train.csv', 'test.csv')
+
+    # encode categorical values
+    encoded_train_matrix, target, test_encoded = encode(df_train, df_test)
+
+    # fit algo.
+    rf = algo_fit_cross_validated(encoded_train_matrix, target)
+
+    # run on test file to predict and report submission file
+    predict_save_file(df_test, test_encoded, rf)
 
 
 if __name__ == "__main__":
